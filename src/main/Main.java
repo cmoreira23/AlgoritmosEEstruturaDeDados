576797f2826135101897d478cefef11b39082ccd
Alex <alexzephiros@hotmail.com>
2019-09-17 18:34:31 -0300

    Adicionando relatório

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,13 +10,13 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         
-        String filePath = new File("").getAbsolutePath() + "/src/data/saidaAleatoria.txt";
+        // String filePath = new File("").getAbsolutePath() + "/src/data/saidaAleatoria.txt";
         // String filePath = new File("").getAbsolutePath() + "/src/data/saidaInvertida.txt";
-        // String filePath = new File("").getAbsolutePath() + "/src/data/saidaOrdenada.txt";
+        String filePath = new File("").getAbsolutePath() + "/src/data/saidaOrdenada.txt";
 
         BufferedReader csvReader = new BufferedReader(new FileReader(filePath));
         String row;
-        int quantity = 500;
+        int quantity = 10000;
         int line = 0;
         int[] array = new int[quantity];        
         while ((row = csvReader.readLine()) != null && line < quantity) {
@@ -25,10 +25,10 @@ public class Main {
         }
         csvReader.close();
 
-       SortHelper helper = new InsertionHelper(quantity); // Trocas e comparações - OK/OK
-//        SortHelper helper = new SelectionSortHelper(quantity); // Trocas e comparações - OK/OK/OK
+      //  SortHelper helper = new InsertionHelper(quantity); // Trocas e comparações - OK/OK
+       SortHelper helper = new SelectionSortHelper(quantity); // Trocas e comparações - OK/OK/OK
 //
-//        SortHelper helper = new QuickSortHelper(quantity, 0, quantity - 1); // Trocas e comparações - OK/OK
+      //  SortHelper helper = new QuickSortHelper(quantity, 0, quantity - 1); // Trocas e comparações - OK/OK
       //  SortHelper helper = new ShellSortHelper(quantity); // Trocas e comparações - OK/OK/OK
         // SortHelper helper = new HeapSortHelper(quantity); // Trocas e comparações - OK/OK
         helper.setArray(array);
